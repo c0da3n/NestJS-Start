81660867fff706b49c599bbb7af88046fbf044cb
c0da3n <sky@codawn.dev>
2025-03-31 08:43:05 -0400

    chore: Add entities

--- src/shared/database.module.ts
+++ src/shared/database.module.ts
@@ -14,6 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         username: config.get<string>('DB_USERNAME'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
+		entities: [],
         synchronize: true,
       }),
     }),
