09f0ef9d58425440cec27b9c5d85c9a1f7dca889
c0da3n <sky@codawn.dev>
2025-03-31 08:44:47 -0400

    style: Change code format

--- src/shared/database.module.ts
+++ src/shared/database.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         username: config.get<string>('DB_USERNAME'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
-		entities: [],
+        entities: [],
         synchronize: true,
       }),
     }),
